267e227c886e9aeccfee52ee23ad5f48582efea6
tomcarney <thomas.carney@griffithuni.edu.au>
2020-04-18 16:55:37 +1000

    myHeap implemented for max heap. (still have to do digit sum for other half of the problem.)

--- main.cpp
+++ main.cpp
@@ -3,10 +3,132 @@
 #include <vector>
 
 
+template<typename t>
+
+class myPQueue{
+private:
+    t *nums;
+    int curSize{}, capacity{};
+
+public:
+    explicit myPQueue(int capacity): nums{new t[capacity]}, capacity{capacity} {}
+    myPQueue(): myPQueue(10) {}
+    myPQueue(myPQueue const & that): capacity{that.capacity}, nums{new t[that.capacity]} {}
+    myPQueue(myPQueue && that) noexcept: capacity{std::move(that.capacity)}, nums{std::move(that.nums)} {}
+    myPQueue & operator = (myPQueue const & that) noexcept {
+        if(this == &that){
+            return *this;
+        }
+        capacity = that.capacity;
+        nums = that.nums;
+        return *this;
+    }
+    myPQueue & operator = (myPQueue && that) noexcept {
+        capacity = std::move(that.capacity);
+        nums = std::move(that.nums);
+        that.nums = nullptr;
+        return *this;
+    }
+    ~myPQueue(){delete [] nums;}
+
+    t &operator [] (int i){
+        if(i > curSize || i < 0){
+            std::cerr << "Index out of queue." << std::endl;
+            return nums[0];
+        } else {
+            return nums[i];
+        }
+    }
+
+    void print(){
+        for(int i = 0; i < curSize; i++){
+            std::cout << nums[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    int size(){
+        return curSize;
+    }
+
+    int getParent(int i){
+        return (i-1)/2;
+    }
+
+    int getLeft(int i){
+        return (i*2)+1;
+    }
+
+    int getRight(int i){
+        return (i*2)+2;
+    }
+
+    void push(int num){
+        if(curSize == capacity){
+            std::cerr << "Error: Not enough space." << std::endl;
+            return;
+        }
+
+        int i = this->curSize;
+        this->curSize++;
+
+        nums[i] = num;
+
+        while((i && nums[getParent(i)] < nums[i])){
+            int temp = nums[i];
+            nums[i] = nums[getParent(i)];
+            nums[getParent(i)] = temp;
+            i = getParent(i);
+        }
+    }
+
+    int pop(){
+        if(curSize <= 0){
+            std::cerr << "No items left in queue." << std::endl;
+            return 0;
+        }
+        if(curSize == 1){
+            curSize--;
+            return nums[0];
+        }
+
+        //keep top number;
+        int temp = nums[0];
+        //replace root with last number;
+        nums[0] = nums[curSize-1];
+        curSize--;
+
+        //fix heap
+        myHeapify(0);
+
+        return temp;
+    }
+
+    void myHeapify(int i){
+        int left = getLeft(i);
+        int right = getRight(i);
+        int largest = i;
+
+        if(left < curSize && nums[getLeft(i)] > nums[i]){
+            largest = left;
+        }
+        if(right < curSize && nums[right] > nums[largest]){
+            largest = right;
+        }
+        if(largest != i){
+            int temp = nums[i];
+            nums[i] = nums[largest];
+            nums[largest] = temp;
+            myHeapify(largest);
+        }
+    }
+};
+
+
 class BallGame{
 private:
     int numBalls{}, maxTurns{};
-    std::vector<int> nums;
+    myPQueue<int> *nums{};
     bool isHeads{};
 
 public:
@@ -14,6 +136,7 @@ public:
 
     void setupGame(std::ifstream &inputFile){
         std::string s;
+        //run for loop for each set up step.
         for(int j = 0; j < 3; j++){
             std::getline(inputFile, s);
             //set a marker to keep track of where we are up to for the string input.
@@ -26,26 +149,23 @@ public:
                 this->maxTurns = stoi(s.substr(mark));
             }else if(j == 1){
                 //step 2 init.
+                nums = new myPQueue<int>[numBalls];
                 mark = 0;
                 //set the marker to the front of the string
                 for(int k = 0; k < this->numBalls; k++){
-                    nums.push_back(stoi(s, &mark));
+                    nums->push(stoi(s, &mark));
                     s = s.substr(mark);
                 }
-            }else{
+            }else {
                 //if heads, isHeads == true;
                 this->isHeads = s == "HEADS";
             }
         }
-        std::cout << this->numBalls << " ";
-        std::cout << this->maxTurns << std::endl;
-        for(auto a : nums){
-            std::cout << a << " ";
-        }
-        std::cout << std::endl;
-        std::cout << this->isHeads << std::endl;
     }
 
+    void findWinner(){
+        //TODO complete this
+    }
 };
 
 
@@ -61,11 +181,11 @@ int main(int argc, char *argv[]) {
 
     //run for loop for every test case.
     for(int i = 0; i < testCases; i++) {
-        //run while loop for each input step
         //TODO remove this
         std::cout << "Test: " << i + 1 << std::endl;
 
         BallGame currentGame(inputFile);
+        //currentGame.findWinner();
     }
 
     inputFile.close();
